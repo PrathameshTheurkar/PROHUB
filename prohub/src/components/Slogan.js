a79a4d87fbb10c39091f49e9b38af57af88bd2d6
Prathamesh <prathameshtheurkar037@gmail.com>
2023-09-29 17:52:17 +0530

    Frontend

--- prohub/src/components/Slogan.js
+++ prohub/src/components/Slogan.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "C:/Users/PRATHAMESH THEURKAR/OneDrive/Desktop/PROHUB/prohub/src/styles/Slogan.css"
+// import logo from '../assets/pro-hub_logo.png'
 
 const Slogan= () =>{
     return  (
@@ -7,11 +8,13 @@ const Slogan= () =>{
             <div className = "Slogan-mid">
             <div className= "Slogan1"><h1>Student Code</h1></div>
             <div className= "Slogan2"><h1>INNOVATION HUB</h1></div>
+            <div className = "Slogan3">“ CODE | COLLABORATE | INNOVATE | TRIUMPH “</div>
             <div className='Description'>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit cum corporis labore nisi eum? Saepe qui necessitatibus libero aliquid sint impedit ipsam debitis!</p>
+                <p>A platform for Tech enthusiasts to work, explore and collaborate with wizards from multiple domains and  ultimately brush an astonishing touch to their innovation </p>
+                <p className= 'Slogan4'>“ WHERE TALENT MEETS OPPORTUNITY “</p>
             </div>
             <div className = "Explore">
-            <button>Explore Now</button>
+            <button className="explore-btn">Explore Now</button>
             </div>
             </div>
         </div>
---
+++ prohub/src/components/cards.js
@@ -0,0 +1,20 @@
+import React from "react"
+import '../styles/cards.css'
+
+const Cards = () =>{
+    return(
+        <div className="card">
+      <div className="card--head">CHAT APP</div>
+      <div className="card--des">
+        Lorem ipsum dolor sit amet consectetur. Ullamcorper gravida faucibus
+        mauris fringilla enim tellus iaculis. Aliquet gravida pulvinar lectus
+        vestibulum feugiat mi. Feugiat in quis id lorem tempor et bibendum.
+        Aliquet dui posuere egestas et volutpat non nulla. Amet nec mattis
+        sit....
+      </div>
+      <div className="card--update">Last Date: 22 Oct 2023</div>
+    </div>
+    );
+}
+
+export default Cards;
